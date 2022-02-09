d6dc242af225b5198a87ad9a1b7057ac405cb8cc
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-02-08 20:01:57 -0500

    commit tree ecac31beec461101128265dd13103d41cb326a5a

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(function (response) {
+    return renderBooks(response);
+  })
 }
 
 function renderBooks(books) {
